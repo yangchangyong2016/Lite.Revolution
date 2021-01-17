6b1ba2689d7dcda22334a678319aef5f03bd8b2b
yangchangyong2016 <40517402+yangchangyong2016@users.noreply.github.com>
2021-01-17 20:30:32 +0800

    Add files via upload

---
+++ Loginxaml/Login.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lite.Revolution.Loginxaml
+{
+	/// <summary>
+	/// Login.xaml 的交互逻辑
+	/// </summary>
+	public partial class Login : Window
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
+
+		private void Min_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = WindowState.Minimized;
+		}
+
+		private void Close_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void Dock_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.LeftButton == MouseButtonState.Pressed)
+			{
+				try
+				{
+					DragMove();
+				}
+				catch (Exception)
+				{
+
+				}
+				
+			}
+		}
+	}
+}
